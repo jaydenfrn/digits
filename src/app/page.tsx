59c9d03fea10b7a9805f7bd218d3dbb97db5685e
Jayden Francoise <jaydenfrancoise@gmail.com>
2025-10-30 23:14:34 -1000

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,51 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Calendar2CheckFill, FileEarmarkTextFill, PeopleFill } from 'react-bootstrap-icons';
 
 /** The Home page. */
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
+      <Container>
+        <Row className="align-middle text-center">
+          <Col>
+            <PeopleFill size="100" />
+            <br />
+            <strong>
+              Multiple Users
+              <br />
+            </strong>
+            <text>
+              This address book enables any number of users to register and save their business contacts.
+              You can only see the contacts you have created.
+            </text>
+          </Col>
 
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Col>
-      </Row>
+          <Col>
+            <FileEarmarkTextFill size="100" />
+            <br />
+            <strong>
+              Contact Details
+              <br />
+            </strong>
+            <text>
+              For each contact, you can save their name, address, and phone number.
+            </text>
+          </Col>
+
+          <Col>
+            <Calendar2CheckFill size="100" />
+            <br />
+            <strong>
+              Timestamped Details
+              <br />
+            </strong>
+            <text>
+              Each time you make contact with a contact, you can write a note that summarizes the conversation.
+              This note is saved along with a timestamp with the contact.
+            </text>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   </main>
 );
